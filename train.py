63a8e1f6846411b50744e07dd03af50556eceffe
rjeks <rjekstein@gmail.com>
2021-06-23 21:56:56 -0300

    Initial Mlops commit

--- train.py
+++ train.py
@@ -5,13 +5,11 @@ from sklearn import svm, datasets
 import joblib 
 import numpy as np
 
-#Load data
+
 iris = datasets.load_iris()
 X = iris.data  
 y = iris.target
 
-#Train model with all data 
 model = svm.SVC(kernel='poly', degree=3, C=1.0).fit(X, y)
 
-#Save mod
 joblib.dump(model, "modelo.joblib")
